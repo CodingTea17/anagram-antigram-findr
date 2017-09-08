2f16db848f7da39000cb7cd14211a22112b9ed6c
Dawson <dawson.mortenson@gmail.com>
2017-09-08 16:15:27 -0700

    add code to validate words by checking for the presence of vowels

--- lib/anagram_findr.rb
+++ lib/anagram_findr.rb
@@ -1,22 +1,27 @@
 class Grams
   def initialize(word)
-    @word_ary = word.upcase.split("").delete_if {|char| char == " " or char == ","}
-
-    @reverse_ary = []
+    # Takes a word in when the object is created and proceeds to format the word
+    @word = word.upcase.gsub(/[^a-zA-Z]/,'').strip.split("")
+    @reverse = []
   end
   def anagram(word)
-    candidate_ary = word.upcase.split("").delete_if {|char| char == " " or char == ","}
-    if candidate_ary.sort.join.casecmp(@word_ary.sort.join) === 0
-      (@word_ary.length-1).downto(0) do |i|
-        @reverse_ary.push(@word_ary[i])
-      end
-      if @reverse_ary.join.casecmp(candidate_ary.join) === 0
-        "These words are palindromes and anagrams!"
+    # Formats the word we are checking to be UPCASE, have no punctuation, and be an array of chars
+    candidate = word.upcase.gsub(/[^a-zA-Z]/,'').strip.split("")
+    if @word.join.scan(/[aeiouy]/i).length == 0 or candidate.join.scan(/[aeiouy]/i).length == 0
+       "Fake word detected..Please enter real word"
+     else
+      if candidate.sort.join.casecmp(@word.sort.join) === 0
+        (@word.length-1).downto(0) do |i|
+          @reverse.push(@word[i])
+        end
+        if @reverse.join.casecmp(candidate.join) === 0
+          "These words are palindromes and anagrams!"
+        else
+          "These words are anagrams!"
+        end
       else
-        "These words are anagrams!"
+        "These words are not anagrams :("
       end
-    else
-      "These words are not anagrams :("
     end
   end
 end
--- spec/anagram_findr_spec.rb
+++ spec/anagram_findr_spec.rb
@@ -6,19 +6,23 @@ RSpec.describe("Grams#anagram") do
     expect(ruby.anagram("bury")).to eq("These words are anagrams!")
   end
   it("will check to see if 'bury' is an anagram of 'cat'") do
-    ruby = Grams.new("cat")
-    expect(ruby.anagram("bury")).to eq("These words are not anagrams :(")
+    cat = Grams.new("cat")
+    expect(cat.anagram("bury")).to eq("These words are not anagrams :(")
   end
   it("will check to see if 'Bury' is an anagram of 'Ruby'") do
     ruby = Grams.new("Ruby")
     expect(ruby.anagram("Bury")).to eq("These words are anagrams!")
   end
-  it("will check to see if 'Kayak' is an anagram and a palidrome of 'Kayak'") do
-    ruby = Grams.new("Kayak")
-    expect(ruby.anagram("Kayak")).to eq("These words are palindromes and anagrams!")
+  it("will check to see if 'Kayak' (a palindrome with a capitalized letter) is an anagram and a palidrome of itself") do
+    kayak = Grams.new("Kayak")
+    expect(kayak.anagram("Kayak")).to eq("These words are palindromes and anagrams!")
   end
-  it("will check to see if 'No lemons, no melon' is an anagram and a palidrome of 'Kayak'") do
-    ruby = Grams.new("No lemons, no melon")
-    expect(ruby.anagram("No lemons, no melon")).to eq("These words are palindromes and anagrams!")
+  it("will check to see if 'No lemons, no melon' (a palindrome phrase with punctuation) is an anagram and a palidrome of itself") do
+    lemons = Grams.new("No lemons, no melon")
+    expect(lemons.anagram("No lemons, no melon")).to eq("These words are palindromes and anagrams!")
+  end
+  it("will check to see if 'gddglc' ('google' with no vowesl) is word by checking for vowls") do
+    gdddlc = Grams.new("gddglc")
+    expect(gdddlc.anagram("lddggc")).to eq("Fake word detected..Please enter real word")
   end
 end
