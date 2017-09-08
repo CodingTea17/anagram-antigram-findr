8a5a8bc54663c07ec651dd13e2e4d5f3f3c691e0
Dawson <dawson.mortenson@gmail.com>
2017-09-08 14:58:11 -0700

    change the way the anagram mehtod was checking for anagrams to include words that are the same as well as code&test to check for a palindrome

--- lib/anagram_findr.rb
+++ lib/anagram_findr.rb
@@ -1,10 +1,23 @@
 class Grams
   def initialize(word)
-    @word_ary = word.split("")
+    @word_ary = word.upcase.split("")
+    @reverse_ary = []
   end
-  def findr(word)
-    if (@word_ary & word.split("")).length == (@word_ary).length
-      "These words are anagrams!"
+  def anagram(word)
+    candidate_ary = word.upcase.split("")
+    # return (candidate_ary & @word_ary).length == @word_ary.length
+    if candidate_ary.sort == @word_ary.sort
+      @word_ary.delete_if {|char| char == " "}
+      (@word_ary.length-1).downto(0) do |i|
+        @reverse_ary.push(@word_ary[i])
+      end
+      if @reverse_ary.join.casecmp(@word_ary.join) === 0
+        "These words are palindromes and anagrams!"
+      else
+        "These words are anagrams!"
+      end
+    else
+      "These words are not anagrams :("
     end
   end
 end
--- spec/anagram_findr_spec.rb
+++ spec/anagram_findr_spec.rb
@@ -1,8 +1,20 @@
 require "anagram_findr"
 
-RSpec.describe("Grams#anagram_findr") do
+RSpec.describe("Grams#anagram") do
   it("will check to see if 'bury' is an anagram of 'ruby'") do
     ruby = Grams.new("ruby")
-    expect(ruby.findr("bury")).to eq("These words are anagrams!")
+    expect(ruby.anagram("bury")).to eq("These words are anagrams!")
+  end
+  it("will check to see if 'bury' is an anagram of 'cat'") do
+    ruby = Grams.new("cat")
+    expect(ruby.anagram("bury")).to eq("These words are not anagrams :(")
+  end
+  it("will check to see if 'Bury' is an anagram of 'Ruby'") do
+    ruby = Grams.new("Ruby")
+    expect(ruby.anagram("Bury")).to eq("These words are anagrams!")
+  end
+  it("will check to see if 'Kayak' is an anagram and a palidrome of 'Kayak'") do
+    ruby = Grams.new("Kayak")
+    expect(ruby.anagram("Kayak")).to eq("These words are palindromes and anagrams!")
   end
 end
